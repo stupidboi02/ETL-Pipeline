47ff62d85d324184f3bd75b15015c520256a12b1
poetwritecode <datphamdang7602@gmail.com>
2025-01-10 16:33:05 +0700

    feat: add load task

---
+++ spark/load.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+from datetime import datetime
+
+
+jdbc_url = "jdbc:postgresql://data-warehouse:5432/datawarehouse"
+properties = {
+    "user": "datawarehouse",
+    "password": "datawarehouse",
+    "driver": "org.postgresql.Driver"
+}
+def load(classification):
+    existing_data = spark.read.jdbc(url=jdbc_url, table = classification + "_" + runtime, properties=properties)
+    # existing_data.printSchema()  # Kiểm tra schema
+    # existing_data.show()  # Kiểm tra dữ liệu
+    existing_data.write.jdbc(url=jdbc_url, table="google_store_game", mode="append", properties=properties)
+
+if __name__ == '__main__':
+    runtime = datetime.now().strftime('%d%m%y')
+    spark = SparkSession.builder.appName('combine and load') \
+        .config('spark.jars', '/opt/airflow/code/postgresql-42.2.5.jar').getOrCreate()
+    load('game_phone')
+    load('game_tablet')
